406edafdf348c23b86cd1a6784de264046fa5779
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-03-18 15:07:27 +0100

    added a buildscript for examples

---
+++ build_examples.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import subprocess
+
+examples_folder = Path("examples")
+examples_folder.mkdir(exist_ok=True)
+
+bytecode_folder = Path("bytecode")
+bytecode_folder.mkdir(exist_ok=True)
+
+for file in examples_folder.rglob("*.lsd"):
+    return_code = subprocess.run([".build/bin/alby", file, "-c", Path(bytecode_folder, file.with_suffix("").name)])
+    if return_code.returncode:
+        print(" ".join(map(str, return_code.args)))
+        break;
