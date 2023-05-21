90fced4a2f7c62514a777577914bbf0f21e6c2a9
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-05-21 13:45:04 +0200

    Updated the build_examples.py script

--- build_examples.py
+++ build_examples.py
@@ -8,7 +8,7 @@ bytecode_folder = Path("bytecode")
 bytecode_folder.mkdir(exist_ok=True)
 
 for file in examples_folder.rglob("*.lsd"):
-    return_code = subprocess.run([".build/bin/alby", file, "-c", Path(bytecode_folder, file.with_suffix("").name)])
+    return_code = subprocess.run([".build/debug/bin/alby", file, "-c", Path(bytecode_folder, file.with_suffix("").name)])
     if return_code.returncode:
         print(" ".join(map(str, return_code.args)))
         break;
