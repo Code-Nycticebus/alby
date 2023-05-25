820af091e66e5a0fe38a751dc9a01f54736da4c3
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-05-25 12:58:28 +0200

    Created real parser tests to test functionality

--- tests/compiler/parser-test.c
+++ tests/compiler/parser-test.c
@@ -3,137 +3,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../../src/compiler/parser.h"
+#include "compiler/parser.h"
+#include "compiler/result.h"
+#include "interpreter/cpu/cpu_defines.h"
+#include "lexer/lexer.h"
 
-#include "../../src/lexer/lexer.h"
-#include "../../src/lexer/token.h"
-
-#include "../../src/interpreter/cpu/cpu.h"
-#include "../../src/interpreter/cpu/cpu_inst.h"
-
-#define PROGRAM_MAX 100
-
-typedef struct Program {
-  size_t size;
-  CpuInstruction inst[PROGRAM_MAX];
-} Program;
-
-static uint64_t parse_ui64(Token tk) {
-  assert(tk.kind == TOKEN_LIT_I64);
-  const int base = 10;
-  return strtoul(tk.token, NULL, base);
-}
-
-static CpuInstruction parse_mov(Lexer *lexer) {
-  Token op1 = lexer_next(lexer);
-
-  printf("TOKEN: %.*s -> %s\n", (int)op1.len, op1.token,
-         token_kind_to_string(op1.kind));
-  assert(TOKEN_REGISTER_1 <= op1.kind && op1.kind <= TOKEN_REGISTER_SB &&
-         "First operant has to be a register");
-  Token comma = lexer_next(lexer);
-  assert(comma.kind == TOKEN_DEL_COMMA && "Missing comma delimiter");
-  Token op2 = lexer_next(lexer);
-  if (op2.kind == TOKEN_LIT_I64) {
-    return cpu_inst_movi(op1.kind - TOKEN_REGISTER_1, parse_ui64(op2));
-  }
-  if (TOKEN_REGISTER_1 <= op2.kind && op2.kind <= TOKEN_REGISTER_8) {
-    return cpu_inst_movr(op1.kind - TOKEN_REGISTER_1,
-                         op2.kind - TOKEN_REGISTER_1);
-  }
-  return cpu_inst_exit(1);
-}
-
-static CpuInstruction parse_sub(Lexer *lexer) {
-  Token reg = lexer_next(lexer);
-  assert(TOKEN_REGISTER_1 <= reg.kind && reg.kind <= TOKEN_REGISTER_SB &&
-         "first operant has to be a register");
-  Token comma = lexer_next(lexer);
-  assert(comma.kind == TOKEN_DEL_COMMA && "Comma missing");
-  Token second = lexer_next(lexer);
-  if (second.kind == TOKEN_LIT_I64) {
-    return cpu_inst_subi(reg.kind - TOKEN_REGISTER_1, parse_ui64(second));
-  }
-  if (TOKEN_REGISTER_1 <= second.kind && second.kind <= TOKEN_REGISTER_SB) {
-    return cpu_inst_subr(reg.kind - TOKEN_REGISTER_1,
-                         second.kind - TOKEN_REGISTER_1);
-  }
-  return cpu_inst_exit(1);
-}
-
-static CpuInstruction parse_add(Lexer *lexer) {
-  Token reg = lexer_next(lexer);
-  assert(TOKEN_REGISTER_1 <= reg.kind && reg.kind <= TOKEN_REGISTER_SB &&
-         "First operant has to be a register");
-  if (lexer_next(lexer).kind != TOKEN_DEL_COMMA) {
-    assert(0 && "Missing comma delimiter");
-  }
-  // Number or Register
-  Token second = lexer_next(lexer);
-  if (second.kind == TOKEN_LIT_I64) {
-    return cpu_inst_addi(reg.kind - TOKEN_REGISTER_1, parse_ui64(second));
-  }
-  if (TOKEN_REGISTER_1 <= second.kind && second.kind <= TOKEN_REGISTER_SB) {
-    return cpu_inst_addr(reg.kind - TOKEN_REGISTER_1,
-                         second.kind - TOKEN_REGISTER_1);
-  }
-  return cpu_inst_exit(1);
-}
-
-static int parser(size_t len, const char *source_code, Program *program) {
-  Lexer lexer = lexer_init(len, source_code);
-  for (Token tk = lexer_next(&lexer); tk.kind != TOKEN_EOF;
-       tk = lexer_next(&lexer)) {
-    switch (tk.kind) {
-    case TOKEN_MOV: {
-      printf("PARSING MOV\n");
-      CpuInstruction inst = parse_mov(&lexer);
-      assert(inst.operation == CPU_OP_MOVI || inst.operation == CPU_OP_MOVR);
-      program->inst[program->size++] = inst;
-      break;
-    }
-    case TOKEN_OP_SUB: {
-      CpuInstruction inst = parse_sub(&lexer);
-      assert(inst.operation == CPU_OP_SUBI || inst.operation == CPU_OP_SUBR);
-      program->inst[program->size++] = inst;
-      break;
-    }
-    case TOKEN_OP_ADD: {
-      CpuInstruction inst = parse_add(&lexer);
-      assert(inst.operation == CPU_OP_ADDI || inst.operation == CPU_OP_ADDR);
-      program->inst[program->size++] = inst;
-    }
-    case TOKEN_DEL_NEWLINE:
-      break;
-    default:
-      assert(0 && "Unexpected token!");
-      break;
-    }
-  }
-  program->inst[program->size++] = cpu_inst_debug();
-  program->inst[program->size] = cpu_inst_exit(0);
-  return 0;
-}
-
-static int run_program(CpuInstruction *program) {
-  Cpu cpu = {0};
-  return cpu_run_program(&cpu, program);
-}
+#define NEXT(__parser_result, __parser)                                        \
+  do {                                                                         \
+    __parser_result = parser_next(&__parser);                                  \
+    if (__parser.eof == true) {                                                \
+      continue;                                                                \
+    }                                                                          \
+    if (__parser_result.ok == false) {                                         \
+      parser_error_print(&__parser_result.value.err);                          \
+      return -1;                                                               \
+    }                                                                          \
+  } while (0);
 
 int main(void) {
-  const char str[] = "mov r1, 489\n"
-                     "mov r2, 34\n"
-                     "sub r1, r2\n"
-                     "sub r1, 35\n"
+  const char str[] = "mov r2, 34\n"
                      "add r2, 35\n";
 
-  Program program = {0};
+  Parser p = {
+      .filename = "memory",
+      .lexer = lexer_init(sizeof(str), str),
+      .eof = false,
+  };
+  ParserResult pr;
 
-  if (parser(sizeof(str) - 1, str, &program)) {
-    printf("Not succ compile!\n");
-    return 1;
-  }
+  NEXT(pr, p);
+  assert(pr.value.ok.operation == CPU_OP_MOVI &&
+         "Parser did not parse operation correctly");
+  assert(pr.value.ok.value2 == 34 &&
+         "Parser did not parse the integer correctly");
 
-  run_program(program.inst);
+  NEXT(pr, p);
+  assert(pr.value.ok.operation == CPU_OP_ADDI &&
+         "Parser did not parse operation correctly");
+
+  NEXT(pr, p);
+  assert(p.eof == true && "EOF should be reached!");
   return 0;
 }
